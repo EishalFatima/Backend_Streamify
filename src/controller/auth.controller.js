a6f85e472eb02e27f6936f8cda1ceeb33cfc40f5
Eishal Fatima <fatimaeishal@gmail.com>
2026-07-06 18:42:51 +0500

    Add user model and authentication logic

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,5 +1,58 @@
+import User from "../models/User.js";
+import jwt from 'jsonwebtoken'
+
 export async function Signup(req , res){
-res.send('Signup route')
+const {email , password , fullName} = req.body
+try {
+  if(!email || !password   || !fullName ){
+    return res.status(400).json({message : 'All the fields are required.'})
+  } 
+  if(password.length < 6){
+       return res.status(400).json({message : 'password should be atleast 6 characters long.'})
+  } 
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if(!emailRegex.test(email)){
+      return res.status(400).json({
+        message : 'Email is invalid.'
+      })
+  }
+
+  const exsistingUser = await  User.findone({email})
+  if (exsistingUser){
+      return res.status(400).json({
+        message : 'Email already exsists.Please use a different one.'
+      })
+  }
+  const newUser = await User.Create({
+    email ,
+    fullName,
+    password
+  })
+   const token = jwt.sign({userId:newUser._id},process.env.JWT_SECRET_KEY,{
+    expiresIn : '7d'
+   })
+res.cookie('jwt' , token ,{
+    maxAge : 7*24*60*60*1000,
+    httpOnly : true,
+    sameSite : 'strict',
+    secure : process.env.NODE_ENV === 'production'
+})
+res.status(201).json({
+    success : true,
+    user : newUser
+})
+} catch (error) {
+    console.log('Error in signup controller' , error);
+    res.status(500).json({
+       message : 'Internal server error.'
+        
+    })
+}
+
+
+
+
+
 }
 export async function Login(req , res){
 res.send('Login route')
---
+++ src/models/User.js
@@ -0,0 +1,71 @@
+import mongoose, { Types } from 'mongoose'
+import bcrypt, { hash } from "bcryptjs"
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    bio: {
+        type: String,
+        default: ""
+    },
+    profilePic: {
+        type: String,
+        default: ""
+    },
+    nativeLanguage: {
+        type: String,
+        default: ""
+    },
+    learningLanguag: {
+        type: String,
+        default: ""
+    },
+    Location: {
+        type: String,
+        default: ""
+    },
+    isOnBoarded: {
+        type: Boolean,
+        default: false
+    },
+    friends: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ]
+},
+    { timestamps: true }
+)
+
+
+// pre hook to hash the password so that it cant be hacked
+userSchema.pre("save" , async function(next){
+    if(!this.isModified("password")) return next()
+try {
+    const salt  = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password , salt)
+} catch (error) {
+     next(error)
+}
+})
+
+const User = new mongoose.model("User" , userSchema)
+
+export default User;
+
+
+
+
+
